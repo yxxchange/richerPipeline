b613b98687f3388c0f5f2b0fb64cafd8c35979d6
richer <yxxchange@163.com>
2024-10-04 19:21:28 +0800

    修改git ignore

--- pkg/error.go
+++
@@ -1,9 +0,0 @@
-package pkg
-
-import "errors"
-
-var (
-	ErrTypeAssertFailed = errors.New("type assert failed")
-	ErrTypeNotDAG       = errors.New("not a DAG")
-	ErrOutOfRange       = errors.New("out of range")
-)
